41824ef23b66a29a8d9ff3e43876fffa381de3d5
svyat94 <vinogradov.s94@yandex.ru>
2023-07-05 12:46:25 +0300

    flake8

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -63,8 +63,8 @@ DATABASES = {
         # Меняем настройку Django: теперь для работы будет использоваться
         # бэкенд postgresql
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('POSTGRES_DB', 'django'),
-        'USER': os.getenv('POSTGRES_USER', 'django'),
+        'NAME': os.getenv('POSTGRES_DB', 'kittygram'),
+        'USER': os.getenv('POSTGRES_USER', 'kittygram'),
         'PASSWORD': os.getenv('POSTGRES_PASSWORD', ''),
         'HOST': os.getenv('DB_HOST', ''),
         'PORT': os.getenv('DB_PORT', 5432)
